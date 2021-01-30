e3b8ceb7d0d80ae590d4ea3b08a0f66bf5e569f8
Madis <Madis0@users.noreply.github.com>
2021-01-30 14:51:46 +0200

    Naming and dependencies

--- src/main/java/io/github/madis0/OneBar.java
+++ src/main/java/io/github/madis0/OneBar.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package io.github.madis0;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class OneBar implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
--- src/main/java/io/github/madis0/mixin/ExampleMixin.java
+++ src/main/java/io/github/madis0/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example.mixin;
+package io.github.madis0.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
