a4a39f3e9631d7469a20fc4aac9ffac8a0b689d1
Madis <Madis0@users.noreply.github.com>
2021-02-01 19:56:43 +0200

    Self-contained methods, global toggles
    plus automatic disabling on creative and spectator

--- src/main/java/io/github/madis0/mixin/InGameHudMixin.java
+++ src/main/java/io/github/madis0/mixin/InGameHudMixin.java
@@ -23,13 +23,17 @@ public abstract class InGameHudMixin {
 
     MinecraftClient client;
     MatrixStack stack;
-    boolean disableVanilla = false; // TODO: make it work
+    PlayerEntity playerEntity;
+    HungerManager hungerManager;
+
+    boolean showVanilla = false;
+    boolean showOneBar = true;
+    boolean showArmor = true;
 
     int baseStartW;
     int baseEndW;
     int baseStartH;
     int baseEndH;
-
     int xpStartW;
     int xpEndW;
     int xpStartH;
@@ -39,6 +43,8 @@ public abstract class InGameHudMixin {
     public void render(MatrixStack matrixStack, float tickDelta, CallbackInfo info) {
         client = MinecraftClient.getInstance();
         stack = matrixStack;
+        playerEntity = this.getCameraPlayer();
+        hungerManager = playerEntity.getHungerManager();
 
         baseStartW = this.scaledWidth / 2 - 91;
         baseEndW = baseStartW + 182;
@@ -50,95 +56,102 @@ public abstract class InGameHudMixin {
         xpStartH = baseStartH + 28;
         xpEndH = xpStartH + 1;
 
-        PlayerEntity playerEntity = this.getCameraPlayer();
-        HungerManager hungerManager = playerEntity.getHungerManager();
-
-        float rawHealth = playerEntity.getHealth();
-        float maxHealth = playerEntity.getMaxHealth();
-        int health = MathHelper.ceil(rawHealth);
-
-        int maxHunger = 20;
-        int rawFood = hungerManager.getFoodLevel();
-        float saturation = hungerManager.getSaturationLevel(); //TODO: usage TBD
-        int hunger = maxHunger - rawFood;
-
-        int maxArmor = 20;
-        int armor = playerEntity.getArmor();
-
-        int maxAir = playerEntity.getMaxAir();
-        int rawAir = maxAir - playerEntity.getAir();
-        int air = rawAir / 15;
-
-        int xpLevel = playerEntity.experienceLevel;
-        int maxXp = 183;
-        int xp = (int)(playerEntity.experienceProgress * maxXp);
-
-        //int fire = playerEntity.getFireTicks(); //TODO: only returns 0, 1 or -20
-        //debugText("Fire:" + fire);
-
-        resetBar();
-        renderHealth(rawHealth, maxHealth);
-        renderHunger(hunger, maxHunger);
-        renderArmor(armor, maxArmor);
-        renderAir(rawAir, maxAir);
-        renderXp(xp, maxXp, xpLevel);
-        renderText(health, hunger, air);
+        if(showOneBar && !playerEntity.isSpectator() && !playerEntity.isCreative()) renderBar();
+        if(showArmor) armorBar();
     }
 
     //TODO: overwrite only on-demand
 
-    @Overwrite
-    private void renderStatusBars(MatrixStack matrices){
+    @Inject(method = "renderStatusBars", at = @At(value = "INVOKE"), cancellable = true)
+    private void renderStatusBars(MatrixStack matrices, CallbackInfo ci){
+        if(!showVanilla) ci.cancel();
     }
-    @Overwrite
-    public void renderExperienceBar(MatrixStack matrices, int x){
+    @Inject(method = "renderExperienceBar", at = @At(value = "INVOKE"), cancellable = true)
+    public void renderExperienceBar(MatrixStack matrices, int x, CallbackInfo ci){
+        if(!showVanilla) ci.cancel();
     }
 
-    private void resetBar(){
+    private void renderBar(){
+        barBackground();
+        healthBar();
+        hungerBar();
+        airBar();
+        xpBar();
+        barText();
+    }
+
+    private void barBackground(){
         int backgroundColor = 0xFF000000;
 
         DrawableHelper.fill(stack, baseStartW, baseStartH, baseEndW, baseEndH, backgroundColor);
         DrawableHelper.fill(stack, xpStartW, xpStartH, xpEndW, xpEndH, backgroundColor);
     }
 
-    private void renderHealth(float health, float total){
+    private void healthBar(){
+        float rawHealth = playerEntity.getHealth();
+        float maxHealth = playerEntity.getMaxHealth();
+
         int healthColor = 0xFFD32F2F;
-        DrawableHelper.fill(stack, baseStartW, baseStartH, relativeEndW(getPreciseInt(health), getPreciseInt(total)), baseEndH, healthColor);
+        DrawableHelper.fill(stack, baseStartW, baseStartH, relativeEndW(getPreciseInt(rawHealth), getPreciseInt(maxHealth)), baseEndH, healthColor);
     }
 
-    private void renderArmor(int armor, int total){
+    private void armorBar(){
+        int maxArmor = 20;
+        int armor = playerEntity.getArmor();
+
         int armorColor = 0xFFFFFFFF;
-        DrawableHelper.fill(stack, baseStartW, baseStartH - 1, relativeEndW(armor, total), baseStartH, armorColor);
+        DrawableHelper.fill(stack, baseStartW, baseStartH - 1, relativeEndW(armor, maxArmor), baseStartH, armorColor);
     }
 
-    private void renderHunger(int hunger, int total){
+    private void hungerBar(){
+        int maxHunger = 20;
+        int hunger = maxHunger - hungerManager.getFoodLevel();
+        //float saturation = hungerManager.getSaturationLevel(); //TODO: usage TBD
+
         int hungerColor = 0xFF3E2723;
-        DrawableHelper.fill(stack, relativeStartW(hunger, total), baseStartH, baseEndW, baseEndH, hungerColor);
+        DrawableHelper.fill(stack, relativeStartW(hunger, maxHunger), baseStartH, baseEndW, baseEndH, hungerColor);
     }
 
-    private void renderAir(int air, int total){
+    private void airBar(){
+        int maxAir = playerEntity.getMaxAir();
+        int rawAir = maxAir - playerEntity.getAir();
+        int air = rawAir / 15;
+
         int airColor = 0xFF1A237E;
-        DrawableHelper.fill(stack, relativeStartW(air, total), baseStartH, baseEndW, baseEndH, airColor);
+        DrawableHelper.fill(stack, relativeStartW(rawAir, maxAir), baseStartH, baseEndW, baseEndH, airColor);
     }
 
-    private void renderText(int health, int hunger, int air){
+    private void barText(){
+        int health = MathHelper.ceil(playerEntity.getHealth());
+        int maxHunger = 20;
+
+        int hunger = maxHunger - hungerManager.getFoodLevel();
+
+        int maxAir = playerEntity.getMaxAir();
+        int rawAir = maxAir - playerEntity.getAir();
+        int air = rawAir / 15;
+
         String value;
         if (hunger < 1 && air < 1)
             value = String.valueOf(health);
         else if(hunger >= 1 && air < 1)
             value = health + "-" + hunger;
         else
-            value = health + "-a" + air;
+            value = health + "-A" + air;
         int textColor = 0xFFFFFFFF;
         client.textRenderer.draw(stack, value, baseEndW - client.textRenderer.getWidth(value), baseStartH + 1, textColor);
     }
 
-    private void renderXp(int xp, int total, int level){
+    private void xpBar(){
+        int xpLevel = playerEntity.experienceLevel;
+        int maxXp = 183;
+        int xp = (int)(playerEntity.experienceProgress * maxXp);
+
         int xpColor = 0xFF00C853;
         int relativeEndW;
 
-        if(xp < total)
-            relativeEndW = MathHelper.ceil(xpStartW + ((float)(xpEndW - xpStartW) / total * xp));
+        if(xp < maxXp)
+            relativeEndW = MathHelper.ceil(xpStartW + ((float)(xpEndW - xpStartW) / maxXp * xp));
         else
             relativeEndW = xpEndW;
 
@@ -146,7 +159,7 @@ public abstract class InGameHudMixin {
         int textY = xpStartH - 10;
 
         DrawableHelper.fill(stack, xpStartW, xpStartH, relativeEndW, xpEndH, xpColor);
-        client.textRenderer.drawWithShadow(stack, String.valueOf(level), textX, textY, xpColor);
+        client.textRenderer.drawWithShadow(stack, String.valueOf(xpLevel), textX, textY, xpColor);
     }
 
     private int relativeEndW(int value, int total){
