c1b8e72cb077fe388c5d05b270b3b529623b43b6
Madis <Madis0@users.noreply.github.com>
2021-03-17 21:24:18 +0200

    Crash fix
    Partly reverts commit 7ec289209c56d9a18f003656ad2e5cf7885c7757.
    
    Fixes #11

--- src/main/java/io/github/madis0/OneBar.java
+++ src/main/java/io/github/madis0/OneBar.java
@@ -2,11 +2,14 @@ package io.github.madis0;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 
-public class OneBar implements ClientModInitializer {
+public class OneBar implements ModInitializer {
 	@Override
-	public void onInitializeClient() {
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
 		AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
 	}
 }
