aa0e47ef7c02562ba728e3e54059b856a488e863
Madis <Madis0@users.noreply.github.com>
2022-02-25 23:11:03 +0200

    Automatic max armor/food level

--- src/main/java/io/github/madis0/PlayerProperties.java
+++ src/main/java/io/github/madis0/PlayerProperties.java
@@ -127,7 +127,7 @@ public class PlayerProperties {
         absorption = MathHelper.ceil(playerEntity.getAbsorptionAmount());
         hasAbsorption = absorption > 0;
 
-        maxArmor = 20;
+        maxArmor = playerEntity.defaultMaxHealth;;
         armor = playerEntity.getArmor();
         for (ItemStack armorItem : playerEntity.getArmorItems()) {
             if(!armorItem.isOf(Items.ELYTRA))
@@ -147,7 +147,7 @@ public class PlayerProperties {
         }
         isFlyingWithElytra = playerEntity.isFallFlying();
 
-        maxFoodLevel = 20;
+        maxFoodLevel = playerEntity.defaultMaxHealth;
         maxFoodLevelRaw = (float)maxFoodLevel; // Used for saturation calculations
         foodLevel = hungerManager.getFoodLevel();
         hunger = maxFoodLevel - foodLevel;
