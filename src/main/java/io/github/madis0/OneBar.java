72791458a7b4e2edc88a17ae06e5be9bcee0a089
Madis <Madis0@users.noreply.github.com>
2022-03-19 23:53:32 +0200

    Set entrypoint as client again
    because #11 turned out to be not caused by my mod

--- src/main/java/io/github/madis0/OneBar.java
+++ src/main/java/io/github/madis0/OneBar.java
@@ -2,11 +2,11 @@ package io.github.madis0;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
-public class OneBar implements ModInitializer {
+public class OneBar implements ClientModInitializer {
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
