227ca6f4fb49c8ed411adef477573a20fc40eedb
Madis <Madis0@users.noreply.github.com>
2022-05-07 20:25:10 +0300

    Comment change

--- src/main/java/io/github/madis0/PlayerProperties.java
+++ src/main/java/io/github/madis0/PlayerProperties.java
@@ -333,15 +333,13 @@ public class PlayerProperties {
         }
         heldFoodHealthEstimate = (int) Math.ceil(heldFoodHealthEstimateRaw);
 
-        rawMaxWardenDanger = 140; //7 sec in ticks - the actual max is 12, but it fluctuates above 7 while in radius
+        rawMaxWardenDanger = 140; //7 sec in ticks - the actual max is 13 sec, but it fluctuates above 7 while in radius
         maxWardenDanger = 20;
 
         if(playerEntity.hasStatusEffect(StatusEffects.DARKNESS)){
             rawWardenDanger = playerEntity.getStatusEffect(StatusEffects.DARKNESS).getDuration();
             wardenDanger = Math.min(rawWardenDanger / 7, 20);
         }
-
-        var aa = MinecraftClient.getInstance().getSoundManager();
     }
 
     public static void SetPlayerBurningOnSoulFire(boolean isBurning){
