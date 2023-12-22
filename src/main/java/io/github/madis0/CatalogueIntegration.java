5cbfee58d793ca5a1d096684008c3452baa8142e
Madis <Madis0@users.noreply.github.com>
2023-12-22 17:07:04 +0100

    Update CatalogueIntegration.java

--- src/main/java/io/github/madis0/CatalogueIntegration.java
+++ src/main/java/io/github/madis0/CatalogueIntegration.java
@@ -4,9 +4,6 @@ import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.client.gui.screen.Screen;
 
-/**
- * Provides support for configuring options through Catalogue.
- */
 @SuppressWarnings("unused")
 public final class CatalogueIntegration {
 
