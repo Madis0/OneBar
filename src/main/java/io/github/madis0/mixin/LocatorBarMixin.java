8fc9c7aa6bee0797f16bc649266410fb08ae7f3a
Madis <Madis0@users.noreply.github.com>
2025-06-02 22:35:41 +0200

    Something shows on screen:tm:

--- src/main/java/io/github/madis0/mixin/LocatorBarMixin.java
+++ src/main/java/io/github/madis0/mixin/LocatorBarMixin.java
@@ -7,11 +7,39 @@ import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = LocatorBar.class, priority = 800)
 public abstract class LocatorBarMixin {
-    @Inject(method = "renderBar", at = @At(value = "TAIL"), cancellable = true)
+
+/*
+    @ModifyArg(
+            method = "renderBar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/render/RenderTickCounter;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIII)V"
+            ),
+            index = 5
+    )
+    private int modifyHeightBar(int original) {
+        var clientProperties = new io.github.madis0.ClientProperties();
+        return clientProperties.baseStartH + 30;
+    }*/
+
+    @ModifyVariable(
+            method = "renderAddons(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/render/RenderTickCounter;)V",
+            at = @At("STORE"), // Could also use "LOAD" depending on when you want to intercept
+            index = 3
+    )
+    private int modifyHeightAddons(int original) {
+        var clientProperties = new io.github.madis0.ClientProperties();
+        return clientProperties.baseStartH - 30;
+    }
+
+
+    /*@Inject(method = "renderBar", at = @At(value = "TAIL"), cancellable = true)
     private void hideBarCompat(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci){
         if(MixinConfigQuery.isCompatModeEnabled() && MixinConfigQuery.isOneBarEnabled())
             ci.cancel();
@@ -33,5 +61,5 @@ public abstract class LocatorBarMixin {
     private void hideAddons(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci){
         if(!MixinConfigQuery.isCompatModeEnabled() && MixinConfigQuery.isOneBarEnabled())
             ci.cancel();
-    }
+    }*/
 }
